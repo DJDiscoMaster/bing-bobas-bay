0dc17014d56c26f3df06a7a7b76bcbd146aa893b
DJDiscoMaster <97910541+DJDiscoMaster@users.noreply.github.com>
2025-10-02 01:23:33 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/bing-bobas-bay-main/", // 👈 repo name goes here
   server: {
     host: "::",
     port: 8080,
